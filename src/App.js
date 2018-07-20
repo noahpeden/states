b27a84567cb6fb0cae7341e321a7b51cda6bbd99
Noah Peden <noahpeden@gmail.com>
2018-07-20 15:23:47 -0600

    Boilerplate divs and image set up.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,62 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+	font-size: 30px;
+	font-weight: 500;
+	font-family: oswald, sans-serif;
+	text-align: center;
+	color: #2e4a9e;
+`;
+
+const QuizContainer = styled.div`
+	border: 1px solid black;
+	width: 600px;
+	height: 600px;
+	margin: 0 auto;
+`;
+
+const Question = styled.p`
+	text-align: center;
+	font-weight: 600;
+	font-size: 24px;
+	padding: 0;
+`;
+
+const Choices = styled.li`
+	list-style: none;
+`;
+const Image = styled.img`
+	border: 1px dotted red;
+	margin: 5%;
+	max-height: 630px;
+	width: auto;
+`;
+
+const ProgressTracker = styled.div`
+	width: 95%;
+	border: 1px dotted green;
+	height: 20px;
+`;
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<Title>
+					How Well Do You Know State Capitals? Take This Quiz!
+				</Title>
+				<QuizContainer>
+					<Question />
+					<Image src="https://images.definition.org/wp-content/uploads/2017/03/37626_28.jpg?tr=w-1000,dpr-2,h-630,c-at_max" />
+					<Choices>Choice 1</Choices>
+					<Choices>Choice 2</Choices>
+					<Choices>Choice 3</Choices>
+					<Choices>Choice 4</Choices>
+					<ProgressTracker />
+				</QuizContainer>
+			</div>
+		);
+	}
 }
 
 export default App;
