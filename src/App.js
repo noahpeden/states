ae95ed9c9a8fd11d5a5a79dc3431c2df25e4dacd
Noah Peden <noahpeden@gmail.com>
2018-07-22 21:38:57 -0600

    Correct cities rendering.

--- src/App.js
+++ src/App.js
@@ -12,33 +12,64 @@ import stateCapitals from './data/state-capitals.json';
 
 class App extends Component {
 	state = {
-		question: 2,
-		stateName: ''
+		question: 1,
+		stateName: '',
+        answer: '',
+        cities: []
 	};
-	getStateName = () => {
+	getStateNameAndAnswer = () => {
 		stateCapitals.forEach(region => {
-			if (this.state.question === region['Number']) {
+			this.state.question === region['Number'] &&
 				this.setState({
-					stateName: region['State']
+					stateName: region['State'],
+					answer: region['Name']
 				});
-			}
 		});
-    };
-    componentDidMount(){
-        this.getStateName()
+	};
+
+	getStateChoices = () => {
+		cityData.forEach(state => {
+            this.state.stateName === state['state'] &&
+                console.log('state', state.cities)
+                this.setState({
+                    cities: state.cities
+                })
+				// state['cities'].forEach(city => <p>{city}</p>);
+		});
+	};
+
+	componentDidMount() {
+        this.getStateNameAndAnswer();
+        this.getStateChoices()
+    }
+    
+    renderCities = () => {
+        
+        this.state.cities.map((city) => {
+            <ul>
+                <li>{city}</li>
+            </ul>
+        })
     }
 
 	render() {
-		const { question } = this.state;
+		const { question, stateName, answer } = this.state;
+        console.log('Answer', answer);
+        console.log('city', this.state.cities)
 		return (
 			<div className="App">
 				<Title>
 					How Well Do You Know State Capitals? Take This Quiz!
 				</Title>
 				<QuizContainer>
-					<Question>What is the capital of {this.state.stateName}?</Question>
-
-					<Choices />
+					<Question>
+						What is the capital of {this.state.stateName}?
+					</Question>
+                    <Choices>
+                        {this.state.cities.map((city)=> {
+                            return <div>{city}</div>
+                        })}
+                    </Choices>
 					<ProgressTracker />
 				</QuizContainer>
 			</div>
