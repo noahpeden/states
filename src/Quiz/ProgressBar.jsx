6d55dc8336291bf4ff6e33330fffbcb7961ddad3
Noah Peden <noahpeden@gmail.com>
2018-07-29 20:16:37 -0600

    Small refactor.

--- src/Quiz/ProgressBar.jsx
+++ src/Quiz/ProgressBar.jsx
@@ -1,19 +1,15 @@
 import React from 'react';
 
-const ProgressBar = props => {
-    return (
-        <div>
-            <h3>Your Progress</h3>
-            <div className="progress-bar">
-                <Filler percentage={props.percentage} />
-                <h3>{props.percentage}%</h3>
-            </div>
+const ProgressBar = props => (
+    <div>
+        <h3>Your Progress</h3>
+        <div className="progress-bar">
+            <Filler percentage={props.percentage} />
+            <h3>{props.percentage}%</h3>
         </div>
-    );
-};
+    </div>
+);
 
-const Filler = props => {
-    return <div style={{ width: `${props.percentage}%` }} className="filler" />;
-};
+const Filler = props => <div style={{ width: `${props.percentage}%` }} className="filler" />;
 
 export default ProgressBar;
